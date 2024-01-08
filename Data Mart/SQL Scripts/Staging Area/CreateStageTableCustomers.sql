abba187599332905d8fd344e7c4f11ba1769d9f3
vitorbarbosa1221412 <1221412@isep.ipp.pt>
2024-01-08 17:16:19 +0000

    Files update

--- Data Mart/SQL Scripts/Staging Area/CreateStageTableCustomers.sql
+++ Data Mart/SQL Scripts/Staging Area/CreateStageTableCustomers.sql
@@ -1,8 +1,8 @@
 IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'Customers') 
 	CREATE TABLE [dbo].[Customers]
 	(
-		[CustomerID] [int],
-		[PersonID] [int]	
+		[CustomerID] [int] NOT NULL,
+		[PersonID] [int] NULL	
 	)
 ELSE
 	TRUNCATE TABLE Customers
