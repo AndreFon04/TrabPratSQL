a51d297f58817036512d8d474fdcbe78770a0e6e
vitorbarbosa1221412 <1221412@isep.ipp.pt>
2024-01-09 00:53:23 +0000

    files update

--- Data Mart/SQL Scripts/Staging Area/CreateStageTableSalesOrderHeaders.sql
+++ Data Mart/SQL Scripts/Staging Area/CreateStageTableSalesOrderHeaders.sql
@@ -1,7 +1,7 @@
 IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'SalesOrderHeaders') 
 	CREATE TABLE [dbo].[SalesOrderHeaders](
 		[SalesOrderID] [int] NOT NULL,
-		[OrderDate] [date] NOT NULL,
+		[OrderDate] [datetime] NOT NULL,
 		[DueDate] [date] NOT NULL,
 		[ShipDate] [date] NOT NULL,
 		[CustomerID] [int] NOT NULL,
